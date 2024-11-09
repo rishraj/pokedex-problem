cdf40f474d375f42fe02889f4a85d10d4330a503
Rishav Raj <rishav.raj2309@gmail.com>
2024-11-09 20:32:07 +0530

    add vercelpostgres compatibility

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,17 @@
 import { publicProcedure, router } from "./trpc";
+import prisma from "@/lib/prisma";
 
 // Create an instance of trpc router as appRouter
 // Define your APIs
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
-    return [10, 20, 30, 40, 50];
+    const pokemon = await prisma.pokemon.findUnique({
+      where: {
+        name: 'Charizard',
+      }
+    })
+
+    return pokemon;
   }),
 });
 
